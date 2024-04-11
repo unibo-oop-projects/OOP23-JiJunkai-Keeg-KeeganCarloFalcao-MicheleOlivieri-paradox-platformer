9bf965b9c19d3e4cc5ddaf55893e694b36e57d93
Ji Junkai <junkai.ji@studio.unibo.it>
2024-04-11 14:09:38 +0200

    fix Collidable import issue

--- src/main/java/com/project/paradoxplatformer/utils/collision/api/Collidable.java
+++ src/main/java/com/project/paradoxplatformer/utils/collision/api/Collidable.java
@@ -1,5 +1,13 @@
-package com.project.paradoxplatformer.model;
+package com.project.paradoxplatformer.utils.collision.api;
+
+import com.project.paradoxplatformer.utils.world.Dimension;
+import com.project.paradoxplatformer.utils.world.Point;
 
 public interface Collidable {
 
+    Point getPosition();
+
+    Dimension getSize();
+
+    void handleCollision(Collidable other);
 }

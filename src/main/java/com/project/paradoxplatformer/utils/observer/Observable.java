1a607ed11e83eb058eaefb931ea0f6f1d6fa7b85
Ji Junkai <junkai.ji@studio.unibo.it>
2024-04-14 00:20:24 +0200

    fix Observable import issue

--- src/main/java/com/project/paradoxplatformer/utils/observer/Observable.java
+++ src/main/java/com/project/paradoxplatformer/utils/observer/Observable.java
@@ -1,5 +1,7 @@
 package com.project.paradoxplatformer.utils.observer;
 
+import com.project.paradoxplatformer.utils.world.observer.Observer;
+
 public interface Observable {
     void addObserver(Observer observer);
 

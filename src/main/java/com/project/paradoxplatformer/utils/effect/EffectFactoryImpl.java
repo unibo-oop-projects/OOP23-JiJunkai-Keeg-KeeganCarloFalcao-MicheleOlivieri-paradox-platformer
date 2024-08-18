d8e590d37c6242a848da130d028e70bd52ff5b5e
Keegan Carlo Falcao <keegancarlo.falcao@studio.unibo.it>
2024-08-18 22:40:12 +0100

    improved

--- src/main/java/com/project/paradoxplatformer/utils/effect/EffectFactoryImpl.java
+++ src/main/java/com/project/paradoxplatformer/utils/effect/EffectFactoryImpl.java
@@ -3,15 +3,18 @@ package com.project.paradoxplatformer.utils.effect;
 import java.util.concurrent.CompletableFuture;
 
 import com.project.paradoxplatformer.model.obstacles.Coin;
+import com.project.paradoxplatformer.model.obstacles.CollectableGameObject;
 import com.project.paradoxplatformer.model.player.PlayerModel;
 import com.project.paradoxplatformer.utils.effect.api.Effect;
 
 public class EffectFactoryImpl implements EffectsFactory {
 
     @Override
-    public Effect collectingEffect(final PlayerModel p) {
+    public Effect collectingEffect(final PlayerModel player) {
         return t -> CompletableFuture.runAsync(() -> {
-            t.filter(Coin.class::isInstance).ifPresent(c -> p.collectCoin());
+            t.filter(CollectableGameObject.class::isInstance)
+                .map(CollectableGameObject.class::cast)
+                .ifPresent(player::collectCoin);
         });
     }
 

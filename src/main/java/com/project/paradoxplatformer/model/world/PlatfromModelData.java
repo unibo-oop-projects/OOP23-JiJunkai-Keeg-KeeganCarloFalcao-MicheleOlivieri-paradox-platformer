bc6218d5b236ef84d0b1fb2c7c9f9a7437f04a39
Keegan Carlo Falcao <keegancarlo.falcao@studio.unibo.it>
2024-08-26 23:36:28 +0100

    simple

--- src/main/java/com/project/paradoxplatformer/model/world/PlatfromModelData.java
+++ src/main/java/com/project/paradoxplatformer/model/world/PlatfromModelData.java
@@ -22,68 +22,78 @@ import com.project.paradoxplatformer.utils.effect.EffectHandler;
 import com.project.paradoxplatformer.utils.geometries.Dimension;
 
 import static java.util.function.Predicate.not;
-
+                                                
 public final class PlatfromModelData implements GameModelData {
 
-        private final LevelDTO packedData;
-        private final WorldBuilder worldBuilder;
-        private World world;
-        private final ModelMappingFactory modelFactory;
+    private final LevelDTO packedData;
+	private final WorldBuilder worldBuilder;
+	private World world;
+	private final ModelMappingFactory modelFactory;
 
-        public PlatfromModelData(final LevelDTO packedData) {
-                this.packedData = packedData;
-                this.modelFactory = new ModelMappingFactoryImpl();
-                this.worldBuilder = new WordBuilderImpl();
-        }
+	public PlatfromModelData(final LevelDTO packedData) {
+		this.packedData = packedData;
+		this.modelFactory = new ModelMappingFactoryImpl();
+		this.worldBuilder = new WordBuilderImpl();
+	}
 
-        // COULD BETTER PERFORM
-        @Override
-        public void init() {
-                Optional.of(
-                                Arrays.stream(packedData.getGameDTOs())
-                                                .map(GameDTO::getType)
-                                                .anyMatch(Objects::isNull))
-                                .filter(u -> !u)
-                                .orElseThrow(() -> new IllegalStateException(
-                                                "Attribute type of game DTO is undefined, could not map"));
+	// COULD BETTER PERFORM
+	@Override
+	public void init() {
+		Optional.of(
+			Arrays.stream(packedData.getGameDTOs())
+				.map(GameDTO::getType)
+				.anyMatch(Objects::isNull)
+		)
+		.filter(u -> !u)
+		.orElseThrow(() -> new IllegalStateException("Attribute type of game DTO is undefined, could not map"));
 
-                this.world = this.worldBuilder
-                                .addbounds(new Dimension(packedData.getWidth(), packedData.getHeight()))
-                                .addPlayer(modelFactory.playerToModel().map(
-                                                this.findGameDTOData("player")
-                                                                .stream()
-                                                                .findFirst()
-                                                                .orElseThrow()))
-                                .addObstacle(
-                                                this.findGameDTOData("obstacle").stream()
-                                                                .map(modelFactory.obstacleToModel()::map)
-                                                                .toList()
-                                                                .toArray(new Obstacle[0]))
-                                .addTrigger(
-                                                this.findGameDTOData("trigger").stream()
-                                                                .map(modelFactory.triggerToModel()::map)
-                                                                .toList()
-                                                                .toArray(new Trigger[0]))
-                                .build();
-        }
+		this.world = this.worldBuilder
+			.addbounds(new Dimension(packedData.getWidth(), packedData.getHeight()))
+			.addPlayer(
+				modelFactory.playerToModel()
+				.map(this.findGameDTOData("player")
+				.stream()
+				.findFirst()
+				.orElseThrow())
+			)
+			.addObstacle(
+				this.findGameDTOData("obstacle")
+				.stream()
+				.map(modelFactory.obstacleToModel()::map)
+				.toList()
+				.toArray(new Obstacle[0])
+			)
+			.addTrigger(
+				this.findGameDTOData("trigger")
+				.stream()
+				.map(modelFactory.triggerToModel()::map)
+				.toList()
+				.toArray(new Trigger[0])
+			)
+			.build();
+	}
 
-        private Collection<GameDTO> findGameDTOData(final String attribute) {
-                return Optional.of(Set.of(packedData.getGameDTOs()).stream()
-                                .filter(g -> g.getType().equals(attribute))
-                                .toList())
-                                .filter(not(List::isEmpty))
-                                .orElseThrow(
-                                                () -> new IllegalArgumentException(
-                                                                "attribute does not match any game dto type: "
-                                                                                + attribute));
-        }
+	private Collection<GameDTO> findGameDTOData(final String attribute) {
+			return Optional.of(
+					Set.of(packedData.getGameDTOs())
+						.stream()
+						.filter(g -> g.getType().equals(attribute))
+						.toList()
+				)
+				.filter(not(List::isEmpty))
+				.orElseThrow(
+					() -> new IllegalArgumentException(
+									"attribute does not match any game dto type: " + attribute
+					)
+				);
+	}
 
-        /**
-         * Due to security reasons returning world must be defensive copy of itself
-         */
-        @Override
-        public World getWorld() {
-                return new WorldImpl(this.world);
-        }
+	/**
+	 * Due to security reasons returning world must be defensive copy of itself
+	 */
+	@Override
+	public World getWorld() {
+		return new WorldImpl(this.world);
+	}
 
 }

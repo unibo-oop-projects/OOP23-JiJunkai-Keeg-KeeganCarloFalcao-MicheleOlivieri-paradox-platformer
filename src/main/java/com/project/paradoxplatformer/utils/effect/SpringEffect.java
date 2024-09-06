7f7dfb03eb8be3ba8c50389c9b1fe29406de212e
Ji Junkai <junkai.ji@studio.unibo.it>
2024-09-07 01:19:47 +0200

    improve effects

--- src/main/java/com/project/paradoxplatformer/utils/effect/SpringEffect.java
+++ src/main/java/com/project/paradoxplatformer/utils/effect/SpringEffect.java
@@ -1,40 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.project.paradoxplatformer.utils.effect;
 
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import com.project.paradoxplatformer.model.entity.CollidableGameObject;
 import com.project.paradoxplatformer.model.entity.dynamics.ControllableObject;
+import com.project.paradoxplatformer.utils.effect.api.RecreateableEffect;
 
-
-public class SpringEffect extends AbstractEffect{
+public class SpringEffect extends AbstractRecreatableEffect {
 
     public SpringEffect() {
     }
 
     @Override
-    protected CompletableFuture<Void> applyToGameObject(CollidableGameObject gameObject) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public RecreateableEffect recreate() {
+        return this;
     }
 
-    
     @Override
-    protected CompletableFuture<Void> applyToTarget(Optional<? extends CollidableGameObject> target) {
+    protected CompletableFuture<Void> applyToGameObject(CollidableGameObject gameObject) {
         return CompletableFuture.runAsync(() -> {
-            target.ifPresent(t -> {
-                if (t instanceof ControllableObject) {
-                    ((ControllableObject) t).jump();
-                }
-            });
-        //Puoi fare anche cosi
-        // target.filter(ControllableObject.class::isInstance)
-        //       .map(ControllableObject.class::cast)
-        //       .ifPresent(ControllableObject::jump);
+            if (gameObject instanceof ControllableObject) {
+                ((ControllableObject) gameObject).jump();
+            }
         });
     }
 }

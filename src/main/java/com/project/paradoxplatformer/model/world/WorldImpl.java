5181d647ecd4e29795af735f82e75c0157e2fadc
Ji Junkai <junkai.ji@studio.unibo.it>
2024-09-14 23:15:47 +0200

    add javadoc

--- src/main/java/com/project/paradoxplatformer/model/world/WorldImpl.java
+++ src/main/java/com/project/paradoxplatformer/model/world/WorldImpl.java
@@ -11,6 +11,12 @@ import com.project.paradoxplatformer.model.world.api.World;
 import com.project.paradoxplatformer.utils.SecureWrapper;
 import com.project.paradoxplatformer.utils.geometries.Dimension;
 
+/**
+ * Implementation of the World interface, representing the game world
+ * in the Paradox Platformer game. This class maintains collections of
+ * obstacles, triggers, and provides access to the player model and world
+ * dimensions.
+ */
 public final class WorldImpl implements World {
 
     private final Set<Obstacle> obstacles;
@@ -18,6 +24,15 @@ public final class WorldImpl implements World {
     private final SecureWrapper<PlayerModel> player;
     private final Dimension bounds;
 
+    /**
+     * Constructs a WorldImpl instance with specified obstacles, triggers, player
+     * model, and world dimensions.
+     *
+     * @param obstacles The collection of obstacles in the world.
+     * @param triggers  The collection of triggers in the world.
+     * @param player    The player model representing the player in the world.
+     * @param bounds    The dimensions (bounds) of the world.
+     */
     public WorldImpl(final Collection<Obstacle> obstacles, final Collection<Trigger> triggers, final PlayerModel player,
             final Dimension bounds) {
         this.obstacles = new LinkedHashSet<>(obstacles);
@@ -26,6 +41,11 @@ public final class WorldImpl implements World {
         this.bounds = bounds;
     }
 
+    /**
+     * Copy constructor to create a new WorldImpl instance from an existing World.
+     *
+     * @param copy The World instance to copy.
+     */
     public WorldImpl(final World copy) {
         this(copy.obstacles(),
                 copy.triggers(),
@@ -43,7 +63,12 @@ public final class WorldImpl implements World {
         return Collections.unmodifiableCollection(this.triggers);
     }
 
-    // unmodifiable
+    /**
+     * Retrieves the player model in the world.
+     * The player model is accessed securely.
+     *
+     * @return The player model.
+     */
     @Override
     public PlayerModel player() {
         return this.player.get();
@@ -54,21 +79,31 @@ public final class WorldImpl implements World {
         return this.bounds;
     }
 
+    /**
+     * Removes a mutable object from the world. The object can be either a trigger
+     * or an obstacle.
+     *
+     * @param mutableGameObj The mutable object to remove.
+     * @return true if the object was successfully removed; false otherwise.
+     */
     @Override
     public boolean removeGameObjects(final MutableObject mutableGameObj) {
         if (mutableGameObj instanceof Trigger) {
             return this.triggers.remove(mutableGameObj);
         } else if (mutableGameObj instanceof Obstacle) {
-
-            var y = this.obstacles.remove(mutableGameObj);
-            return y;
+            return this.obstacles.remove(mutableGameObj);
         }
         return false;
     }
 
+    /**
+     * Retrieves a collection of all mutable objects in the world, including
+     * obstacles, triggers, and the player model.
+     *
+     * @return A collection of mutable objects.
+     */
     @Override
     public Collection<MutableObject> gameObjects() {
         return Sets.union(Sets.union(this.obstacles, this.triggers), Set.of(this.player()));
     }
-
 }

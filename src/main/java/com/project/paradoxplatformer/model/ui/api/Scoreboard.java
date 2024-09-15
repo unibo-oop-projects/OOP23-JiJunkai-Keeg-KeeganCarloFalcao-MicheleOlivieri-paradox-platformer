2c315707e108a1537349085713326dbdae28fbf8
Ji Junkai <junkai.ji@studio.unibo.it>
2024-09-15 17:00:56 +0200

    remove unused menu

--- src/main/java/com/project/paradoxplatformer/model/ui/api/Scoreboard.java
+++
@@ -1,9 +0,0 @@
-package com.project.paradoxplatformer.model.ui.api;
-
-public interface Scoreboard {
-    void updateTimePassed(long time);
-
-    void incrementDeathCounter();
-
-    void display();
-}
